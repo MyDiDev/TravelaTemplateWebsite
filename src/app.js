7e5b28f081d81c943b7f501ea79e49fb254b137d
MyDiDev <didev4207@gmail.com>
2026-03-02 19:22:30 -0400

    add proyect files to repo

---
+++ src/app.js
@@ -0,0 +1,41 @@
+import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = process.env?.port || 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/blog", (req, res) => {
+  res.render("blog");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.get("/services", (req, res) => {
+  res.render("services");
+});
+
+app.use((req, res) => {
+  res.status(404).render("404");
+});
+
+app.listen(port, () => {
+  console.log(`listening on http://localhost:${port}`);
+});
